30da5e24a6f6dc1955ee4d3cc0790f1c4f4be03d
sumonrst <eng.sumonrejata@gmail.com>
2024-11-22 07:13:30 +0600

    header sticky done

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+$(window).on('scroll', function (){
+    if($(window).scrollTop() > 500){
+        $('.header_area').addClass('sticky');
+    }else{
+        $('.header_area').removeClass('sticky');
+    }
+});
